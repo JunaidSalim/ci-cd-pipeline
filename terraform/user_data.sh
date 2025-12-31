c09d9d4b561e8b978d280a3b79270fb2b171d052
Junaid Saleem <junaidsaleem986@gmail.com>
2025-12-31 18:12:48 +0500

    Updated Script

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get upgrade -y
+while fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
+  echo "Waiting for other software managers to finish..."
+  sleep 5
+done
 
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl gnupg lsb-release unzip
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/apt/sources.list.d/docker.list > /dev/null
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin awscli
 
 sudo systemctl start docker
 sudo systemctl enable docker
-
 sudo usermod -aG docker ubuntu
+
+mkdir -p /home/ubuntu/app
+chown ubuntu:ubuntu /home/ubuntu/app
